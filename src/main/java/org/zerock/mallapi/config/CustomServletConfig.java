b2af128c9b7b2e5c1bec081a168cc11a11f2f780
SY Jung <104877247+ha7708@users.noreply.github.com>
2026-02-12 12:36:50 +0900

    GET, POST, PUT, DELETE 컨트롤러, CORS 설정

--- src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
+++ src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -2,6 +2,7 @@ package org.zerock.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.mallapi.controller.formatter.LocalDateFormatter;
 
@@ -11,4 +12,26 @@ public class CustomServletConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new LocalDateFormatter());
     }
+
+    // CORS 설정법은 2가지가 있다.
+    // 1. @Controller 가 있는 클래스에 @CrossOrigin 적용 : 모든 컨트롤러에 개별적으로 적용해야 한다.
+    // 2. SpringSecurity 를 이용하는 설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+        // 주의점 1
+        // Spring Security가 켜져 있으면 CORS는 Security 필터 체인에서 먼저 걸러질 수 있음
+        // → 그럼 MVC의 addCorsMappings가 제대로 적용 안 되는 케이스가 많다
+        // → 이 경우 Security 설정에서 http.cors() 활성화, CorsConfigurationSource 빈 제공
+        // 주의점 2
+        // 
+
+    }
+
+
 }
--- src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -2,14 +2,13 @@ package org.zerock.mallapi.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.mallapi.dto.PageRequestDTO;
 import org.zerock.mallapi.dto.PageResponseDTO;
 import org.zerock.mallapi.dto.TodoDTO;
 import org.zerock.mallapi.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +16,14 @@ import org.zerock.mallapi.service.TodoService;
 @RequestMapping("/api/todo")
 public class TodoController {
     private final TodoService service;
+
+    // POSTMAN 테스트를 위해 추가
+    // 요청시에는 POST방식, http://localhost:8080/api/todo/ 로 요청
+    @PostMapping("/")
+    public Long register(@RequestBody TodoDTO todoDTO) {
+        log.info("register...." + todoDTO);
+        return service.register(todoDTO);
+    }
 
     @GetMapping("/{tno}")
     public TodoDTO get(@PathVariable(name = "tno") Long tno) {
@@ -28,4 +35,15 @@ public class TodoController {
         log.info(pageRequestDTO);
         return service.list(pageRequestDTO);
     }
+
+    // Todo 수정 : PUT 방식
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+        log.info("Modify: " + todoDTO);
+        service.modify(todoDTO);
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+
 }
